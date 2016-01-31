5fb7b74fd707388b670fd37437b04966a4255b51
David Maack <david.maack@arcor.de>
2016-01-31 11:42:01 +0100

    Updated copyright notice

--- system/modules/templateSelection/languages/de/tl_theme.php
+++ system/modules/templateSelection/languages/de/tl_theme.php
@@ -1,30 +1,15 @@
-<?php if (!defined('TL_ROOT')) die('You cannot access this file directly!');
+<?php
 
 /**
  * Contao Open Source CMS
- * Copyright (C) 2005-2012 Leo Feyer
+ * Copyright (c) 2005-2015 Leo Feyer
  *
- * Formerly known as TYPOlight Open Source CMS.
- *
- * This program is free software: you can redistribute it and/or
- * modify it under the terms of the GNU Lesser General Public
- * License as published by the Free Software Foundation, either
- * version 3 of the License, or (at your option) any later version.
- * 
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
- * Lesser General Public License for more details.
- * 
- * You should have received a copy of the GNU Lesser General Public
- * License along with this program. If not, please visit the Free
- * Software Foundation website at <http://www.gnu.org/licenses/>.
- *
- * PHP version 5
- * @copyright  MEN AT WORK 2012
  * @package    templateSelection
- * @license    GNU/GPL 2
- * @filesource
+ * @copyright  MEN AT WORK 2016
+ * @author     Andreas Isaak <andy.jared@googlemail.com>
+ * @author     David Maack <david.maack@arcor.de>
+ * @author     Patrick Kahl <kahl.patrick@googlemail.com>
+ * @license    LGPL-3.0+
  */
 
 /**
